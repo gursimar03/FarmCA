672fbdc2a40219fcf4c8996a0a6fa2cf831c831e
gursimar03 <49545220+gursimar03@users.noreply.github.com>
2022-11-30 12:39:07 +0000

    init

--- FarmCA/BeefCow.java
+++ FarmCA/BeefCow.java
@@ -2,14 +2,9 @@ package FarmCA;
 
 public class BeefCow extends Animal {
 
-    private int milkProduction;
 
     public BeefCow(int id, String name) {
         super(id, name);
-    }
-
-    public int getMilkProduction() {
-        return milkProduction;
     }
 
     @Override
