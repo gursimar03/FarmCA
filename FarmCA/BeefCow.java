f25b7633121413570ab2196c3d46aa9ada2d7056
gursimar03 <gursimar03@gmail.com>
2022-12-10 14:03:30 +0000

    init

--- FarmCA/BeefCow.java
+++ FarmCA/BeefCow.java
@@ -2,10 +2,46 @@ package FarmCA;
 
 public class BeefCow extends Animal {
 
+    private int pedgree;
+    private int weight;
+    private int age;
 
     public BeefCow(int id, String name) {
         super(name);
     }
+
+    public BeefCow(int id, String name, int pedgree, int weight, int age) {
+        super(name);
+        this.pedgree = pedgree;
+        this.weight = weight;
+        this.age = age;
+    }
+
+    public int getPedgree() {
+        return pedgree;
+    }
+
+    public void setPedgree(int pedgree) {
+        this.pedgree = pedgree;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    
 
     public BeefCow() {
         
